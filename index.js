4a5295f146775c49bdaf6567a445c54cdc3324ae
yurii <pavlykyurii15@gmail.ru>
2022-11-05 00:21:14 +0200

    Interchangeable

---
+++ index.js
@@ -0,0 +1,11 @@
+let personsAge = '23';
+console.log(personsAge)
+
+let isAdultperson = 'true';
+
+let programName;
+console.log(programName)
+programName = 'C++';
+
+const isPersonHaveHomeAnAnimal = 'no'
+
